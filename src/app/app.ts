792c4a9b0228677f08ddb4725fab35943324b162
vCentDev <vicentDev@yahoo.com>
2026-09-11 07:28:24 +0200

    chore: Ejercicio 10 completado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { NgStyle } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { email } from '@angular/forms/signals';
 @Component({
   selector: 'app-root',
   imports: [ReactiveFormsModule, NgStyle],
@@ -11,9 +18,11 @@ import { debounceTime, distinctUntilChanged } from 'rxjs';
 })
 export class App {
   protected readonly title = signal('Formularios Reactivos');
-  userForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
+  private readonly fb = inject(FormBuilder);
+
+  protected readonly userForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
   });
 
   constructor() {
